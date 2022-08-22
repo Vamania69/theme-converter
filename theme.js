43bf08bcecc630897f93c1215740fdc2fd2f768b
usernamr <varunagrawal6261@gmail.com>
2022-08-22 23:59:21 +0530

    initial commit

---
+++ theme.js
@@ -0,0 +1,14 @@
+const checked= document.getElementById('checkbox');
+
+checked.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('italic');
+    document.header.classList.toggle('dark');
+// const checked= document.getElementById('checkbox');
+
+}
+)
+const checkedOne=document.getElementById('theme-1')
+checkedOne.addEventListener('click',()=>{
+    document.body.classList.toggle('theme-1');}
+)
